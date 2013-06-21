699c5a0f53cfc33df2029442c8ff82de4f5764cb
Ben Rathbun <brathbun@burstmedia.com>
2013-06-21 11:28:46 -0400

    5th

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,70 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+
+ // DOMContentLoaded
+
+//Main Filter JS
+$(function() {
+    // bind dropdowns in the form
+    var $filterchannel = $('#filter select[name="channel"]');
+    var $filtermedium = $('#filter select[name="medium"]');
+    var $filterproduct = $('#filter select[name="product"]');
+
+    // get the first collection
+    var $applications = $('#applications');
+
+    // clone applications to get a second collection
+    var $data = $applications.clone();
+
+    // attempt to call Quicksand on every form change
+    $('select').change(
+        function() {
+            $(this).addClass('animate');
+            if ($($filterchannel).val() == '0'){
+                if ($($filtermedium).val() == '0'){
+                    if ($($filterproduct).val() == '0'){
+                        //0-0-0
+                        var $filteredData = $data.find('div');
+                    } else {
+                        //0-0-1
+                        var $filteredData = $data.find('div[data-product=' + $($filterproduct).val() + ']' );
+                    }
+                } else {
+                    if ($($filterproduct).val() == '0'){
+                        //0-1-0
+                        var $filteredData = $data.find('div[data-medium=' + $($filtermedium).val() + ']' );
+                    } else {
+                        //0-1-1
+                        var $filteredData = $data.find('div[data-medium=' + $($filtermedium).val() + ']' + 'div[data-product=' + $($filterproduct).val() + ']');
+                    }
+                }
+            } else {
+                if ($($filtermedium).val() == '0'){
+                    if ($($filterproduct).val() == '0'){
+                        //1-0-0
+                        var $filteredData = $data.find('div[data-channel=' + $($filterchannel).val() + ']' );
+                    } else {
+                        //1-0-1
+                        var $filteredData = $data.find('div[data-channel=' + $($filterchannel).val() + ']' + 'div[data-product=' + $($filterproduct).val() + ']');
+                    }
+                } else {
+                    if ($($filterproduct).val() == '0'){
+                        //1-1-0
+                        var $filteredData = $data.find('div[data-channel=' + $($filterchannel).val() + ']' + 'div[data-medium=' + $($filtermedium).val() + ']');
+                    } else {
+                        //1-1-1
+                        var $filteredData = $data.find('div[data-channel=' + $($filterchannel).val() + ']' + 'div[data-medium=' + $($filtermedium).val() + ']' + 'div[data-product=' + $($filterproduct).val() + ']');
+                    }
+                }
+            }
+            
+            // finally, call quicksand
+            $applications.quicksand($filteredData, {
+                duration: 1000,
+                easing: 'easeInOutQuad',
+                adjustHeight:   'auto'
+        });
+    });
+});
